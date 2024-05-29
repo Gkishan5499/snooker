ecd67de9393df4a7d937d32eb577ea46693755d9
omprakash30 <oy45833@gmail.com>
2024-05-29 16:47:22 +0530

    second commit

--- src/component/Header.js
+++ src/component/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
   return (
 
     <div className='sticky header top-0 z-10  text-3xl flex justify-between text-red-600 font-bold p-3 border-b-2 border-gray-500'>
-     <Link to={'/'}><span>Filmy <span className='text-white'>World</span></span></Link> 
+     <Link to={'/'}><span>Snooker <span className='text-white'>World</span></span></Link> 
      {useAppstate.login?
 
      <Link to={"/addmovie"}><h1 className='text-lg cursor-pointer'><Button><AddIcon className='mr-2 ' color='secondary'/> 
